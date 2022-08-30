898e8a5c868cc2cb73905abdbd510d0c52ac8fff
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-30 15:43:22 -0300

    resolução de exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+
+let numeroPar = Number(prompt("Insira um número par:"))
+console.log(`resto da divisão = ${numeroPar % 2}`)
+// O resto da divisão de um número par por 2 é sempre 0
+// o resto da divisão de um numero impar por 2 é sempre diferente de 0
---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const conta1 = 5 > 20 && 5 < 2 
+const conta2 = 5 === 5 || 5 === "5"
+const conta3 =  !(20 > 50)
+const conta4 = !(20 > 50 || 50 > 60)
+
+console.log(`
+    conta1 = ${conta1}
+    conta2 = ${conta2}
+    conta3 = ${conta3}
+    conta4 = ${conta4}
+    `)
